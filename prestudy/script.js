73ef483251327ead22e3648c9956d5716e493af9
Eugene2374 <136548906+Eugene2374@users.noreply.github.com>
2025-03-22 01:44:03 +0800

    Delete prestudy directory

--- prestudy/script.js
+++
@@ -1,9 +0,0 @@
-Consent=document.getElementById("Info")
-
-ZIn=()=>{
-    Consent.style.width=(parseInt(Consent.style.width.slice(0,2))*1.1).toString()+"%"
-}
-
-ZOut=()=>{
-    Consent.style.width=(parseInt(Consent.style.width.slice(0,2))/1.1).toString()+"%"
-}
